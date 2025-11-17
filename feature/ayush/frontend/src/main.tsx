d4b23ae9493c4385ba08e9d52594ea25b719e3e0
AyushGowdaK9910 <ayushgowdak9910@gmail.com>
2025-11-17 13:46:45 +0530

    feat(CON-12): integrate API docs into app navigation
    - Add API docs route
    - Create navigation link
    - Add docs page styling
    - Integrate with main app layout

--- feature/ayush/frontend/src/main.tsx
+++ feature/ayush/frontend/src/main.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { ApiDocsPage } from './pages/api-docs/ApiDocsPage';
+import { App } from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ApiDocsPage />} />
-        <Route path="/api-docs" element={<ApiDocsPage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
